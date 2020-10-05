c2085926c004d17c75d3feece9708a8ad010fa40
keisaki <dimakrivolapov@gmail.com>
2020-10-05 17:03:39 +0400

    feat: switch in function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+const dict = ['','one','two','three','four','five','six','seven','eight','nine'];
+const dict10 = ['','one','twen','thir','for','fif','six','seven','eigh','nine'];
+
 module.exports = function toReadable (number) {
-  let dict = ['one','two','three','four','five','six','seven','eight','nine'];
-  let dict10 = ['one','twen','thir','for','fif','six','seven','eigh','nine'];
+
   if(number == 0) return 'zero';
   let numberParts = [~~(number/100),~~(number/10)%10,number%10];
   if(number<10)
-    return `${dict[numberParts[2]-1]}`;
+    return `${dict[numberParts[2]]}`;
   if(number<100)
   { 
-        switch(number){
+        return tenToNineteen(number);
+  }
+  if(number%100>9&&number%100<20)
+  {
+  return `${dict[numberParts[0]]+' hundred'} `+tenToNineteen(number%100); 
+}
+  return `${dict[numberParts[0]]+' hundred'}${dict10[numberParts[1]]?' '+dict10[numberParts[1]]+'ty':''}${dict[numberParts[2]]?' '+dict[numberParts[2]]:''}`;
+}
+
+function tenToNineteen(number){
+    let numberParts = [~~(number/100),~~(number/10)%10,number%10];
+    switch(number){
         case 10: return 'ten'; break;
         case 11: return 'eleven'; break;
         case 12: return 'twelve'; break;
@@ -18,24 +31,6 @@ module.exports = function toReadable (number) {
         case 17: return 'seventeen'; break;
         case 18: return 'eighteen'; break;
         case 19: return 'nineteen'; break;
-        default: return `${dict10[numberParts[1]-1]}ty${dict[numberParts[2]-1]?' '+dict[numberParts[2]-1]:''}`;
+        default: return `${dict10[numberParts[1]]?dict10[numberParts[1]]+'ty':''}${dict[numberParts[2]]?' '+dict[numberParts[2]]:''}`;
     }
-  }
-  if(number%100<20)
-  {
-    switch(number%100){
-        case 10: return `${dict[numberParts[0]-1]+' hundred'}`+ ' ten'; break;
-        case 11: return `${dict[numberParts[0]-1]+' hundred'}`+' eleven'; break;
-        case 12: return `${dict[numberParts[0]-1]+' hundred'}`+' twelve'; break;
-        case 13: return `${dict[numberParts[0]-1]+' hundred'}`+' thirteen'; break;
-        case 14: return `${dict[numberParts[0]-1]+' hundred'}`+' fourteen'; break;
-        case 15: return `${dict[numberParts[0]-1]+' hundred'}`+' fifteen'; break;
-        case 16: return `${dict[numberParts[0]-1]+' hundred'}`+' sixteen'; break;
-        case 17: return `${dict[numberParts[0]-1]+' hundred'}`+' seventeen'; break;
-        case 18: return `${dict[numberParts[0]-1]+' hundred'}`+' eighteen'; break;
-        case 19: return `${dict[numberParts[0]-1]+' hundred'}`+' nineteen'; break;
-        default: return `${dict[numberParts[0]-1]+' hundred'}${dict10[numberParts[1]-1]?' '+dict10[numberParts[1]-1]+'ty':''}${dict[numberParts[2]-1]?' '+dict[numberParts[2]-1]:''}`;
-  }
-}
-  return `${dict[numberParts[0]-1]+' hundred'}${dict10[numberParts[1]-1]?' '+dict10[numberParts[1]-1]+'ty':''}${dict[numberParts[2]-1]?' '+dict[numberParts[2]-1]:''}`;
 }
